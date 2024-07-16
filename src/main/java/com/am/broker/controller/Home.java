43dc60eb16552261f8b68d783868d896c0079ac9
cuizhengri <cuizhengri@aspnc.com>
2024-07-16 18:06:31 +0800

    重新提交

--- src/main/java/com/am/broker/controller/Home.java
+++ src/main/java/com/am/broker/controller/Home.java
@@ -3,13 +3,18 @@ package com.am.broker.controller;
 
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
 
 import javax.servlet.http.HttpServletRequest;
 
+/**
+ * @author jinyongbin
+ */
 @Controller
-public class ViewController {
+@RequestMapping("/")
+public class Home {
 
-    @GetMapping("/")
+    @GetMapping
     public String index(HttpServletRequest request) {
         return "index";
     }
---
+++ src/main/java/com/am/broker/controller/Platform.java
@@ -0,0 +1,19 @@
+package com.am.broker.controller;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+
+/**
+ * @author jinyongbin
+ * @version 1.0
+ * @since 2024/7/15
+ */
+@Controller
+@RequestMapping("/")
+public class Platform {
+    @GetMapping("plat")
+    public String plat(){
+        return "index";
+    }
+}
