ff24903e534606b8f8fd8d051f92fa9289ea65d1
ybkim <353007811@qq.com>
2024-07-31 15:22:13 +0800

    添加动态激活导航

--- src/main/java/com/am/broker/controller/AboutUs.java
+++ src/main/java/com/am/broker/controller/AboutUs.java
@@ -1,8 +1,12 @@
 package com.am.broker.controller;
 
+import com.am.broker.utils.MessageUtils;
 import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
+
+import java.util.Map;
 
 /**
  * @author jinyongbin
@@ -13,7 +17,9 @@ import org.springframework.web.bind.annotation.RequestMapping;
 @RequestMapping("/")
 public class AboutUs {
     @GetMapping("aboutus")
-    public String contact(){
+    public String contact(Model model){
+        Map<String, String> home = MessageUtils.searchMessages("home");
+        model.addAttribute("home", home);
         return "home/index";
     }
 }
