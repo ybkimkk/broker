cfe7089bc5699863953767589b6730a3f6326c36
ybkim <353007811@qq.com>
2024-08-10 01:03:44 +0800

    更新 语言选择样式

--- src/main/java/com/am/broker/advice/GlobalControllerAdvice.java
+++ src/main/java/com/am/broker/advice/GlobalControllerAdvice.java
@@ -9,8 +9,12 @@ import org.springframework.web.bind.annotation.ExceptionHandler;
 import org.springframework.web.bind.annotation.ModelAttribute;
 import org.springframework.web.servlet.ModelAndView;
 
+import javax.annotation.PostConstruct;
 import javax.servlet.http.HttpServletRequest;
+import java.util.HashMap;
+import java.util.LinkedHashMap;
 import java.util.Map;
+import java.util.Objects;
 
 /**
  * @author jinyongbin
@@ -20,16 +24,39 @@ import java.util.Map;
 
 @ControllerAdvice
 public class GlobalControllerAdvice {
+    private static LinkedHashMap<String, String> languageMap;
+
+    @PostConstruct
+    public void init() {
+        languageMap = new LinkedHashMap<>();
+        languageMap.put("en", "English");
+        languageMap.put("in", "INDONESIA");
+        languageMap.put("vn", "Tiếng Việt");
+        languageMap.put("tc", "繁體中文");
+        languageMap.put("ko", "한국어");
+        languageMap.put("cn", "简体中文");
+        languageMap.put("jp", "日本語");
+        languageMap.put("ar", "بالعربية");
+    }
+
     @ModelAttribute
     public void addAttributes(HttpServletRequest request, Model model) {
-        String lang = ServletUtils.getCookie(request, "lang");
+        String selectedLanguageKey = ServletUtils.getCookie(request, "lang");
+        String selectedLanguageValue = languageMap.get(selectedLanguageKey);
+        if (Objects.isNull(selectedLanguageValue)) {
+            selectedLanguageKey = "en";
+        }
+        for (Map.Entry<String, String> stringStringEntry : languageMap.entrySet()) {
+            model.addAttribute(stringStringEntry.getKey(), stringStringEntry.getValue());
+        }
         Map<String, String> menu = MessageUtils.searchMessages("menu");
         model.addAttribute("menu", menu);
         Map<String, String> footer = MessageUtils.searchMessages("footer");
         model.addAttribute("footer", footer);
         Map<String, String> common = MessageUtils.searchMessages("common");
         model.addAttribute("common", common);
-        model.addAttribute("selectedLanguage", lang);
+        model.addAttribute("selectedLanguageKey", selectedLanguageKey);
+        model.addAttribute("languageMap", languageMap);
     }
 
     @ExceptionHandler(SpelEvaluationException.class)
